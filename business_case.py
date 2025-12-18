4d57bef047ce94db391836dce74953901f35ec8a
sabrin-05 <11sabrin10@gmail.com>
2025-12-18 16:36:50 +0000

    feat: load and read csv datasets

--- business_case.py
+++ business_case.py
@@ -7,3 +7,12 @@ from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
+#~~~~~~~~~~~~~~~~~~~~~~~~
+# load data and read csv
+#~~~~~~~~~~~~~~~~~~~~~~~~
+
+co2_data = pd.read_csv('datasets/co2-emissions-per-capita.csv')
+print(co2_data.head())  # display first few rows of the dataset
+
+electricity_data = pd.read_csv('datasets/electricity-fossil-renewables-nuclear-line.csv')
+print(electricity_data.head())  # display first few rows of the dataset
