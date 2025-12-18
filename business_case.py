85f25a40e75d76bba7901cf4deacd0d82db4e479
sabrin-05 <11sabrin10@gmail.com>
2025-12-18 16:21:10 +0000

    chore: add required libraries imports

---
+++ business_case.py
@@ -0,0 +1,9 @@
+# import necessary libraries
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+from sklearn import tree 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
