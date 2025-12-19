8e42944159a6b9d7eb8d86d6155b15554afa77fb
sabrin-05 <11sabrin10@gmail.com>
2025-12-19 23:09:23 +0000

    feat: add function to clean co2 emissions dataset

--- business_case.py
+++ business_case.py
@@ -11,8 +11,33 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 # load data and read csv
 #~~~~~~~~~~~~~~~~~~~~~~~~
 
+#
 co2_data = pd.read_csv('datasets/co2-emissions-per-capita.csv')
 print(co2_data.head())  # display first few rows of the dataset
 
+#
 electricity_data = pd.read_csv('datasets/electricity-fossil-renewables-nuclear-line.csv')
 print(electricity_data.head())  # display first few rows of the dataset
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# data wrangling and cleaning
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def clean_co2_data(co2_data):
+    '''
+    Function: cleans dataset by dropping columns, dropping NaN values, and filtering for specific years.
+    
+    Returns: cleaned dataframe
+    '''
+
+    # drop unnecessary columns
+    co2_data = co2_data.drop(columns=['Code'])
+
+    # filter for year 2000 onwards
+    co2_data = co2_data[co2_data['Year'] >= 2000]
+
+    return co2_data
+
+co2_data = clean_co2_data(co2_data)
+print(co2_data.head())  # display cleaned data
+
