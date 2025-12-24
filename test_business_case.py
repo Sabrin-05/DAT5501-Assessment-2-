fb500f6c4bfe365afa664cfd8c294ddd552bf0b2
sabrin-05 <11sabrin10@gmail.com>
2025-12-24 23:21:36 +0000

    feat: add unit test class and function to test code

--- test_business_case.py
+++ test_business_case.py
@@ -5,3 +5,11 @@ import numpy as np
 from business_case import clean_co2_data, clean_electricity_data
 import pandas as pd
 from unittest.mock import patch
+
+class my_unit_tests(unittest.TestCase):
+    
+    def test_clean_co2_data(self):
+        '''
+        Function checks that the clean_co2_data function correctly cleans the CO2 dataset.
+
+        '''
