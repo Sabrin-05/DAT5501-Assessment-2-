4883faa65f18a00d321c3b556e6b5b85775db7d7
sabrin-05 <11sabrin10@gmail.com>
2026-01-03 19:53:57 +0000

    feat: add new test for rename column function

--- test_business_case.py
+++ test_business_case.py
@@ -4,6 +4,7 @@ import unittest
 import numpy as np
 from business_case import clean_co2_data
 from business_case import clean_electricity_data
+from business_case import rename_coloumns
 import pandas as pd
 from unittest.mock import patch
 
@@ -49,6 +50,35 @@ class TestCleanCO2Data(unittest.TestCase):
         self.assertNotIn('Code', cleansed.columns) 
         self.assertTrue((cleansed['Year'] >= 2000).all())
 
+    def test_rename_columns(self):
+        '''
+        Function: Checks where the rename columns function in main code is working correctly
+        '''
+
+        # create an example dataframe to mimic the original 
+        test_df = pd.DataFrame({
+        'Entity': ['Canada','Somalia'],
+        'Fossil fuels - % electricity': [0.000987,0.7654567],
+        'Renewables - % electricity': [0.456787654,0.21234],
+        'Nuclear - % electricity': [0.0020123,1.09872],
+        'Annual CO₂ emissions (per capita)': [0.9,0.007]
+        })
+
+        renamed = rename_coloumns(test_df)
+
+        # check new coloumn names exist
+        self.assertIn('country', renamed.columns)
+        self.assertIn('fossil_share', renamed.columns)
+        self.assertIn('renewable_share', renamed.columns)
+        self.assertIn('nuclear_share', renamed.columns)
+        self.assertIn('co2_per_capita', renamed.columns)
+
+        # check old coloumns no longer exist 
+        self.assertNotIn('Entity', renamed.columns)
+        self.assertNotIn('Fossil fuels - % electricity', renamed.columns)
+        self.assertNotIn('Renewables - % electricity', renamed.columns)
+        self.assertNotIn('Nuclear - % electricity', renamed.columns)
+        self.assertNotIn('Annual CO₂ emissions (per capita)', renamed.columns)
     
 if __name__ == "__main__":
     unittest.main()
